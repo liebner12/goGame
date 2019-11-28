d2d984292441f25f37fd3d8198cef17cd3f50c45
Sebastian Maraszek <sebastian.maraszek0@gmail.com>
2019-11-28 19:27:20 +0100

    Added forgotten BlacPiece class.

---
+++ src/main/java/com/michalliebner/sebastianmaraszek/team/gui_swing/ui/BlackPiece.java
@@ -0,0 +1,56 @@
+package com.michalliebner.sebastianmaraszek.team.gui_swing.ui;
+
+import java.awt.Color;
+
+public class BlackPiece implements Piece{
+    private int size=50;
+    private Color color=Color.BLACK;
+    private int posX;
+    private int posY;
+
+    public BlackPiece(){
+
+    }
+
+    @Override
+    public int getX() {
+        return posX;
+    }
+
+    @Override
+    public int getY() {
+        return posY;
+    }
+
+    @Override
+    public void setX(int x) {
+        posX=x;
+    }
+
+    @Override
+    public void setY(int y) {
+        posY=y;
+
+    }
+
+    @Override
+    public int getSize(){
+
+        return size;
+    }
+    @Override
+    public Color getColor()
+    {
+        return color;
+    }
+
+    @Override
+    public void setColor(Color c) {
+        this.color=c;
+    }
+
+    @Override
+    public void setSize(int i) {
+        this.size=i;
+    }
+}
